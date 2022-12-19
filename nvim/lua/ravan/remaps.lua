bf34206d076f46b7afc0e39b51afcd7fcd63954c
rajasekhar vanjarapu <rajasekhar745@gmail.com>
2022-12-19 15:53:44 +0530

    Fix bug in visual mode remaps

--- nvim/lua/ravan/remaps.lua
+++ nvim/lua/ravan/remaps.lua
@@ -6,7 +6,7 @@ local vnoremap = keymap.vnoremap;
 vim.g.mapleader = " ";
 nnoremap("<leader>pv", "<cmd>ex<cr>");
 
-vnoremap("j", ":m '>+1<cr>gv=gv");
-vnoremap("k", ":m '<-2<cr>gv=gv");
+vnoremap("J", ":m '>+1<cr>gv=gv");
+vnoremap("K", ":m '<-2<cr>gv=gv");
 
 -- vim.keymap.set("n", "J", "mzJ`z")

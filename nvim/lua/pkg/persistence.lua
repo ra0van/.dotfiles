29784bb68862ff2f15ff1798cca82621758af0f3
rajasekhar vanjarapu <rajasekhar745@gmail.com>
2023-01-30 16:15:44 +0530

    Disable all LSP & Autocomplete related plugins

--- nvim/lua/pkg/persistence.lua
+++ nvim/lua/pkg/persistence.lua
@@ -1,7 +1,26 @@
 return {
-  event = 'BufReadPre',
-  module = 'persistence',
-  config = function()
-    require'persistence'.setup {}
-  end,
+    event = 'BufReadPre',
+    module = 'persistence',
+    config = function()
+        require'persistence'.setup {}
+    end,
+    keybindings = {
+        n = {
+            {
+                -- restore last session for current directory
+                key = "<leader>qs",
+                lua = "require('persistence').load()"
+            },
+            {
+                -- restore last session
+                key = "<leader>ql",
+                lua = "require('persistence').load({last = true})"
+            },
+            {
+                -- stop persistence => Session won't be saved on exit
+                key = "<leader>qd",
+                lua = "require('persistence').stop()"
+            }
+        }
+    }
 }
